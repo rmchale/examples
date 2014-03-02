84142da3c69c9e41b4d341e56608e55021cdc4c0
rmchale <ryan.mchale@gmail.com>
2014-03-01 23:31:38 -0600

    removed unused vars

--- disruptor/src/test/java/disruptor/DisruptorTest.java
+++ disruptor/src/test/java/disruptor/DisruptorTest.java
@@ -11,17 +11,14 @@ public class DisruptorTest {
 	@Test
 	public void test() throws InterruptedException {
 		SimpleDisruptor disruptor = new SimpleDisruptor();
-		long val1, val2;
 		
 		Data data = disruptor.process(100);
-
 		Data data2 =disruptor.process(101);
 		
 		data.latch.await();
 		data2.latch.await();
 		
 		Assert.assertEquals(202, data.value);
-		
 		Assert.assertEquals(204, data2.value);
 	}
 	
